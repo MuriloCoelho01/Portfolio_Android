0d9c2c0141d46f2ee6831d432d641023910da91c
MuriloCoelho01 <coelho.murilo10@gmail.com>
2025-01-03 21:44:22 -0300

    fix

--- app/src/main/java/com/example/meu_portfolio/MainActivity.kt
+++ app/src/main/java/com/example/meu_portfolio/MainActivity.kt
@@ -14,15 +14,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.meu_portfolio.ui.theme.Meu_portfolioTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,7 +56,7 @@ fun Inicio() {
             verticalArrangement = Arrangement.Top,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = 50.dp)
+                .padding(top = 120.dp)
 
         ) {
        Box(){
@@ -60,7 +64,7 @@ fun Inicio() {
            Box(
                modifier = Modifier
                    .size(width = 170.dp, height = 170.dp)
-                   .border(5.dp, color = Color(0xff0A22BE))
+                   .border(8.dp, color = Color(0xff0A22BE), shape = RoundedCornerShape(20.dp))
            )
            Image(
                painter = painterResource(R.drawable.foto_murilo),
@@ -68,9 +72,30 @@ fun Inicio() {
                modifier = Modifier
                    .size(width = 170.dp, height = 170.dp)
                    .offset(x = -20.dp, y = -20.dp)
+                   .clip(shape = RoundedCornerShape(20.dp))
            )
 
        }
+            Text(
+                "Murilo Coelho",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.White
+
+            )
+            Text(
+                "Desenvolvedor Mobile",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.White
+            )
+            Text(
+                "Crio soluções Mobile acessíveis e gerencio" +
+                        "projetos para entregar resultados eficientes e modernos.",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
         }
     }
 
